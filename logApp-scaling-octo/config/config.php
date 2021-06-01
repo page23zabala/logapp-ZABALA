ded8d429c4d373a20393c734045b1b4b48c84fc3
page23zabala <201880591@psu.palawan.edu.ph>
2021-06-01 22:54:57 +0800

    COMMITTED

--- logApp-scaling-octo/config/config.php
+++ logApp-scaling-octo/config/config.php
@@ -1,6 +1,7 @@
 <?php
-	define('ROOT_URL', '');
-	define('DB_HOST', '');
-	define('DB_USER', '');
-	define('DB_PASS', '');
-	define('DB_NAME', '');
+	define('ROOT_URL', 'guestbook-login.php');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6416382');
+	define('DB_PASS', 'AyCdkW5M1m');
+	define('DB_NAME', 'sql6416382');
+	?>
